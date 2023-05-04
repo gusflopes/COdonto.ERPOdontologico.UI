8a6d228868abb36e1dc26121f5ebf9ced7fccdb3
Gustavo Lopes <gusflopes86@gmail.com>
2023-05-03 23:27:20 -0400

    feat: paginas dentistas e pacientes - inicio

--- src/pages/dashboard/dentistas/index.tsx
+++ src/pages/dashboard/dentistas/index.tsx
@@ -1,7 +1,101 @@
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
+import { useApi } from "../../../hooks/useApi/index";
+
+type Dentista = {
+  id: string;
+  nome?: string;
+  cro?: string;
+  cpf?: string;
+  especialidades?: string;
+  comissao?: number;
+};
+
 export default function Dentistas() {
+  const { getDentistas, updateDentista, createDentista } = useApi();
+  // const api = new useApi();
+  const [dentistas, setDentistas] = useState<Dentista[]>([]);
+
+  const getInitialData = async () => {
+    const dentistas: Dentista[] = await getDentistas();
+    setDentistas(dentistas);
+    console.log(dentistas);
+  };
+
+  const imageUrl =
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60";
+
+  useEffect(() => {
+    getInitialData();
+  }, []);
+
   return (
     <div>
-      <h1>Dentistas</h1>
+      <h1 className="mb-12 text-3xl font-bold">DENTISTAS</h1>
+      <ul
+        role="list"
+        className="grid grid-cols-1 gap-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+      >
+        {dentistas &&
+          dentistas.map((dentista) => (
+            <li
+              key={dentista.id}
+              className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow-2xl shadow-slate-300 drop-shadow"
+            >
+              <div className="flex flex-1 flex-col p-8">
+                <img
+                  className="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
+                  src={imageUrl}
+                  alt=""
+                />
+                <h3 className="mt-6 text-sm font-medium text-gray-900">
+                  {dentista.nome}
+                </h3>
+                <dl className="mt-1 flex flex-grow flex-col justify-between">
+                  <dt className="sr-only">Title</dt>
+                  <dd className="text-sm text-gray-500">
+                    {dentista.especialidades}
+                  </dd>
+                  <dt className="sr-only">Role</dt>
+                  <dd className="mt-3">
+                    <span className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                      Dentista
+                      {/* {dentista.role} */}
+                    </span>
+                  </dd>
+                </dl>
+              </div>
+              <div>
+                <div className="-mt-px flex divide-x divide-gray-200">
+                  <div className="flex w-0 flex-1">
+                    <a
+                      href={`mailto:dentista@hublaw.com.br`}
+                      className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                    >
+                      <EnvelopeIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      Email
+                    </a>
+                  </div>
+                  <div className="-ml-px flex w-0 flex-1">
+                    <a
+                      href={`tel:+5567992638484`}
+                      className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
+                    >
+                      <PhoneIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      Call
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
--- src/pages/dashboard/pacientes/index.tsx
+++ src/pages/dashboard/pacientes/index.tsx
@@ -1,7 +1,160 @@
+import { useEffect, useState } from "react";
+import { useApi } from "../../../hooks/useApi";
+
+type Paciente = {
+  id: string;
+  nome: string;
+  rg: string;
+  cpf: string;
+  dataNascimento: string;
+  email: string;
+  telefone: string;
+  nacionalidade: string;
+  estadoCivil: string;
+  profissao: string;
+  responsavelFinanceiroId: string;
+  responsavelFinanceiro: string;
+  endereco?: Endereco;
+};
+
+type Endereco = {
+  id?: string;
+  pacienteId?: string;
+  cep?: string;
+  logradouro?: string;
+  numero?: string;
+  complemento?: string;
+  bairro?: string;
+  cidade?: string;
+  uf?: string;
+};
+
 export default function Pacientes() {
+  const { getPacientes, createPaciente, updatePaciente } = useApi();
+  const [pacientes, setPacientes] = useState<Paciente[]>([]);
+
+  const getInitialData = async () => {
+    const pacientes: Paciente[] = await getPacientes();
+    setPacientes(pacientes);
+    console.log(pacientes);
+  };
+
+  const imageUrl =
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60";
+
+  useEffect(() => {
+    getInitialData();
+  }, []);
+
   return (
-    <div>
-      <h1>Pacientes</h1>
+    <div className="px-4 sm:px-6 lg:px-8">
+      <h1 className="mb-12 text-3xl font-bold">PACIENTES</h1>
+
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">
+            Users
+          </h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all the users in your account including their name, title,
+            email and role.
+          </p>
+        </div>
+        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <button
+            type="button"
+            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Novo Paciente
+          </button>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Title
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Role
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {pacientes.map((paciente) => (
+                  <tr key={paciente.id}>
+                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                      <div className="flex items-center">
+                        <div className="h-11 w-11 flex-shrink-0">
+                          <img
+                            className="h-11 w-11 rounded-full"
+                            src={imageUrl}
+                            alt=""
+                          />
+                        </div>
+                        <div className="ml-4">
+                          <div className="font-medium text-gray-900">
+                            {paciente.nome}
+                          </div>
+                          <div className="mt-1 text-gray-500">
+                            {paciente.dataNascimento}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      <div className="text-gray-900">{paciente.telefone}</div>
+                      <div className="mt-1 text-gray-500">{paciente.email}</div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                        Active
+                      </span>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      {paciente.responsavelFinanceiro
+                        ? paciente.responsavelFinanceiro
+                        : "-"}
+                    </td>
+                    <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <a
+                        href="#"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        Edit<span className="sr-only">, {paciente.nome}</span>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
